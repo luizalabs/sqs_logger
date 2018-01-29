891e4e6d4741ed4139ca7e079dcf2640d8b620f4
Luiz Gois <luiz.gois@luizalabs.com>
2018-01-29 15:43:06 -0200

    Bump version: 1.0.2 → 1.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup_options = dict(
     name='sqs_logger',
-    version='1.0.2',
+    version='1.0.3',
     description='',
     author='Luiz Gois',
     author_email='luiz.gois@luizalabs.com',
