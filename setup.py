03912e35bf0d49b889e79abbfe973aab35be23a2
Luiz Gois <luiz.gois@luizalabs.com>
2018-01-29 10:12:17 -0200

    fix setup

--- setup.py
+++ setup.py
@@ -1,29 +1,4 @@
-import re
-
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-
-
-with open('requirements/prod.txt') as requirements_file:
-    requirements_file = requirements_file.readlines()
-
-
-DEPENDENCY_LINKS = [
-    link.split()[1] for link in requirements_file
-    if link.startswith('-')
-]
-
-REQUIREMENTS = [
-    requirement for requirement in requirements_file
-    if not requirement.startswith('-')
-]
-
-for link in DEPENDENCY_LINKS:
-    match = re.search('/(\w+).git', link)
-    if match:
-        REQUIREMENTS.append(match.group(1))
+from setuptools import setup
 
 setup_options = dict(
     name='sqs_logger',
@@ -37,9 +12,7 @@ setup_options = dict(
     packages=[
         'sqs_logger',
         'sqs_logger.aws'
-    ],
-    install_requires=REQUIREMENTS,
-    dependency_links=DEPENDENCY_LINKS,
+    ]
 )
 
 setup(**setup_options)
