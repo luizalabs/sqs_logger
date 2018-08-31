92e2c9452318a49d3b82e0a32ec7b5040bc29b07
Jair Henrique <jair.henrique@gmail.com>
2018-08-31 14:25:58 -0300

    Bump version: 1.0.3 → 1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup_options = dict(
     name='sqs_logger',
-    version='1.0.3',
+    version='1.1.0',
     description='',
     author='Luiz Gois',
     author_email='luiz.gois@luizalabs.com',
